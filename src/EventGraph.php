1424f7370a548ed9050cf54de981dff0289e7fd6
Marco Perone <pasafama@gmail.com>
2014-11-27 12:57:01 +0100

    connect to database

--- src/EventGraph.php
+++ src/EventGraph.php
@@ -2,11 +2,20 @@
 
 namespace Marcosh\EventGraph;
 
+use OrientDB\Client;
+
 class EventGraph
 {
-    public function __construct()
-    {
+    private $database;
 
+    public function connect(array $config)
+    {
+        $databaseName = $config['name'];
+        unset($config['name']);
+
+        $client = new Client($config);
+
+        $this->database = $client->getDatabase($databaseName, true);
     }
 
     public function createEvent()
